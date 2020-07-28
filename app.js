58e804f74f0441dc0404c28eb6369790d0e4270a
Daniel Le <lequangduc512014@gmail.com>
2020-07-28 14:08:48 -0700

    modified question

---
+++ app.js
@@ -0,0 +1,100 @@
+let questions = [
+  {
+    "question": "What is the planet we are living",
+    "correct_answer": "Earth",
+    "answers": [
+      "Earth",
+      "Mars",
+      "Pluto",
+      "Juniper"
+    ]
+  },
+  {
+    "question": "What is the tennis player in the world",
+    "correct_answer": "Rafael Nadal",
+    "answers": [
+      "Roger Federer",
+      "Rafael Nadal",
+      "Novak Djokovic",
+      "David Ferrer"
+    ]
+  },
+  {
+    "question": "What is the weather like today",
+    "correct_answer": "Sunny",
+    "answers": [
+      "Rainy",
+      "Cloudy",
+      "Windy",
+      "Sunny"
+    ]
+  },
+  {
+    "question": "Who invented the phone?",
+    "correct_answer": "Graham Bell",
+    "answers": [
+      "Thomas Edison",
+      "Graham Bell",
+      "George Washington",
+      "Richard Nixon"
+    ]
+  },
+  {
+    "question": "Which city is Statue of Liberty located?",
+    "correct_answer": "New York",
+    "answers": [
+      "New York",
+      "Paris",
+      "Wakanda",
+      "San Francisco"
+    ]
+  },
+  {
+    "question": "Whose citadel is Budapest?",
+    "correct_answer": "Turkey",
+    "answers": [
+      "Hungary",
+      "Turkey",
+      "Romania",
+      "Latvia"
+    ]
+  },
+  {
+    "question": "When is the United States of America established?",
+    "correct_answer": "1776",
+    "answers": [
+      "1776",
+      "1885",
+      "1932",
+      "1650"
+    ]
+  },
+  {
+    "question": "Which fruit has a lot of vitamin C?",
+    "correct_answer": "Orange",
+    "answers": [
+      "Banana",
+      "Melon",
+      "Orange",
+      "Watermelon",
+    ]
+  },
+  {
+    "question": "Is Tesla the most promising market stock in the world?",
+    "correct_answer": "True",
+    "answers": [
+      "True",
+      "False"
+    ]
+  },
+  {
+    "question": "How many fingers in a person's hand?",
+    "correct_answer": "5",
+    "answers": [
+      "10",
+      "20",
+      "5",
+      "15"
+    ]
+  }
+]
